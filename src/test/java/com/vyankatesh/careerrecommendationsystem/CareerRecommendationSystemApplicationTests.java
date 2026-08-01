66784159ffb1eceac4c6e25135984da1fd5984f3
Vyanktesh1408 <vyankateshbarkase79@email@gmail.com>
2026-08-01 23:48:07 +0530

    Add initial Spring Boot project setup

---
+++ src/test/java/com/vyankatesh/careerrecommendationsystem/CareerRecommendationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vyankatesh.careerrecommendationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerRecommendationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
